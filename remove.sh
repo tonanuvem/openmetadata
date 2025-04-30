221e126a9b4abb6614de1f91f04a83b5780d13c0
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2025-04-30 11:22:13 -0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 docker-compose stop && docker-compose rm -f && docker volume prune -f && git pull
-
+echo ""
+echo "Removendo volumes:"
 # Lista todos os volumes e remove cada um
 docker volume ls -q | grep openmetadata | while read volume; do
-  echo "Removendo volume: $volume"
+  #echo "Removendo volume: $volume"
   docker volume rm "$volume"
 done
+echo ""
